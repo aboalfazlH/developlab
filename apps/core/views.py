9527a1bbf1fd91160a9a3cbc2be8a54829a30949
aboalfazlH <saboalfazl6162@gmail.com>
2025-11-24 09:56:35 +0330

    Add About and Contact

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,18 @@
 from django.views.generic import TemplateView
 from django.contrib import messages
 from django.shortcuts import render
-
+from apps.accounts.models import CustomUser
 class MainPageView(TemplateView):
     template_name = "index.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["personnels"] = CustomUser.objects.filter(groups__name="کارمندان")
+        return context
+
+class ContactPageView(TemplateView):
+    template_name = "contact.html"
