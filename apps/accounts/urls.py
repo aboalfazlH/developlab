b79bd9814af500111c60ad8c28c908be0457ab82
aboalfazlH <saboalfazl6162@gmail.com>
2025-11-27 11:15:38 +0330

    Add User Change Password

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -7,13 +7,32 @@ from .views import (
     UserDetailView,
     CustomUserUpdateView,
 )
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path("sign-up/", SignUpView.as_view(), name="sign-up"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", CustomLogoutView.as_view(), name="logout"),
-    path("edit/",CustomUserUpdateView.as_view(),name="users-profile-edit",),
+    path(
+        "edit/",
+        CustomUserUpdateView.as_view(),
+        name="users-profile-edit",
+    ),
+    path(
+        "password-change/",
+        auth_views.PasswordChangeView.as_view(
+            template_name="accounts/password_change.html"
+        ),
+        name="password_change",
+    ),
+    path(
+        "password-change/done/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="accounts/password_change_done.html"
+        ),
+        name="password_change_done",
+    ),
     path("<str:username>/", CustomUserDetailView.as_view(), name="users-profile"),
     path("", UserDetailView.as_view(), name="user-profile"),
 ]
