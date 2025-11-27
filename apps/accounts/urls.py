aecc8ee7784001952518d474333e491b4911553b
aboalfazlH <saboalfazl6162@gmail.com>
2025-11-27 11:10:26 +0330

    Add profile edit

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
-from .views import SignUpView,LoginView,CustomLogoutView,CustomUserDetailView,UserDetailView
+from .views import (
+    SignUpView,
+    LoginView,
+    CustomLogoutView,
+    CustomUserDetailView,
+    UserDetailView,
+    CustomUserUpdateView,
+)
 
 
 urlpatterns = [
-    path('sign-up/',SignUpView.as_view(),name="sign-up"),
-    path('login/',LoginView.as_view(),name="login"),
-    path('logout/',CustomLogoutView.as_view(),name="logout"),
-    path('<str:username>/',CustomUserDetailView.as_view(),name="users-profile"),
-    path('',UserDetailView.as_view(),name="user-profile"),
+    path("sign-up/", SignUpView.as_view(), name="sign-up"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("edit/",CustomUserUpdateView.as_view(),name="users-profile-edit",),
+    path("<str:username>/", CustomUserDetailView.as_view(), name="users-profile"),
+    path("", UserDetailView.as_view(), name="user-profile"),
 ]
