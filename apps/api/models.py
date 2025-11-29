e292d3830940cbf9954f6bab250d32e0d8b93429
seyed aboalfazl hashemi <saboalfazl6162@gmail.com>
2025-11-29 16:19:41 +0330

    Add Create Article api

--- apps/api/models.py
+++ apps/api/models.py
@@ -3,7 +3,6 @@ import secrets
 import hashlib
 from apps.accounts.models import CustomUser
 
-
 class ApiModel(models.Model):
     api_name = models.CharField(name="نام رابط")
     key = models.CharField(
@@ -14,7 +13,7 @@ class ApiModel(models.Model):
     )
     user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
     create_date = models.DateTimeField(verbose_name="تاریخ ایجاد",auto_now_add=True)
-    
+    revoked_date = models.DateTimeField(verbose_name="تاریخ بازیابی",blank=True,null=True)
     
     @staticmethod
     def hash_token(raw_token: str) -> str:
@@ -23,6 +22,7 @@ class ApiModel(models.Model):
     def save(self, force_insert = ..., force_update = ..., using = ..., update_fields = ...,*args,**kwargs):
         if not self.key:
             self.key = secrets.token_urlsafe(64)
+            self.hash_token(raw_token=self.key)
         super().save(*args, **kwargs)
     
     @staticmethod
